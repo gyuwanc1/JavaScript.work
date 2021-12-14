5014798be51071339e94aa9571dc1672fb38c03e
b_gyuwan <gyuwanc@gmail.com>
2021-12-15 00:13:23 +0900

    [add] error question

--- js_day/js/5_day.js
+++ js_day/js/5_day.js
@@ -44,3 +44,57 @@ a=200
 b=100
 big =BigNumber(a,b)
 console.log('큰수'+big);
+
+console.clear();
+//---------------------------------------
+
+let printStar = (number) => {
+  if (number<1){
+    console.log('잘못입력');
+    return
+  }
+  let i;
+  for(i=1;i<=number;i++)
+  console.log('*');
+}
+
+printStar(-1);
+printStar(1);
+
+console.clear();
+//===========================================
+
+
+// let multiple = (a,b)=>{
+
+//   if (a <= 0 || b <= 0){
+//     console.log('양수입력')
+//     return
+//   }
+//   if (a % b ===0){
+//     console.log('a는 b의 배수입니다.')
+//   }else {
+//     console.log('a는 b의 배수가 아닙니다')
+//   }  
+// }
+
+// let a=-10 
+// let b=2
+// multiple(a,b); //결과값이  '양수입력'이 나와야한다.
+
+//============================================
+var starX =1;
+function prinstStar(num){
+  for(starX=1; starX <= num; starX++){
+    printStarX(starX);
+  }
+}
+
+function printStarX(num){
+  var starX;
+  for(starX;starX <= num;starX++){
+    console.log('*')
+  }
+}
+printStar(3);
+
