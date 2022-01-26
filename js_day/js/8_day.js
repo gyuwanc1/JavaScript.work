027ba4cce47e88680767f911765c4b6881cb626f
b_gyuwan <gyuwanc@gmail.com>
2022-01-26 22:44:37 +0900

    [add] event mouse JS class study

---
+++ js_day/js/8_day.js
@@ -0,0 +1,19 @@
+// 이벤트 배우기
+
+//1 마우스 이벤트
+
+function overBox(obj){
+  obj.style.backgroundColor="#00BAEF";
+} //마우스가 가면 색변환
+
+function downBox(obj){
+  obj.style.backgroundColor="#88184D";
+}//마우스 왼쪽버튼을 누르면 색변환
+
+function upBox(obj){
+  obj.style.backgroundColor="#fdd";
+}//마우스 왼쪽버튼을 때면 색 변환
+
+function outBox(obj){
+  obj.style.backgroundColor="#171717";
+}//마우스가 해당영역을 나가면 색변환
