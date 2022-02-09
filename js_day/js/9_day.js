84da4f1da3eef4a0db64b9452a899d51f91378b5
b_gyuwan <gyuwanc@gmail.com>
2022-02-10 00:05:12 +0900

    [add]selctor class 2

--- js_day/js/9_day.js
+++ js_day/js/9_day.js
@@ -13,17 +13,45 @@
 
 //태그네임
 
+// {
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue']
+
+// function search(){
+//   var item = document.getElementsByTagName('li'); //배열형식
+//   console.log(item.length)
+
+//   for(let i = 0; i<item.length; i++){
+//     item[i].style.backgroundColor=colors[i];
+//   }
+// }
+
+// }
+
 {
-const colors = ['red', 'orange', 'yellow', 'green', 'blue']
+  function search(){
+    const items = document.getElementsByClassName('selected');
+    console.log('목록수:', items.length)
 
-function search(){
-  var item = document.getElementsByTagName('li'); //배열형식
-  console.log(item.length)
-
-  for(let i = 0; i<item.length; i++){
-    item[i].style.backgroundColor=colors[i];
+    for(let i=0; i < items.length; i++){
+      items[i].style.backgroundColor='orange';
+    }
   }
 }
 
+{
+  function change(){
+    let elms = document.getElementsByClassName('coral')
+    let item = document.querySelectorAll('.coral')
+
+    console.log('[byClassName 전]',elms.length);
+    console.log('[SelectorAll 전]',item.length);
+
+    document.getElementById('third').className='coral';
+
+    console.log('[byClassName 후]',elms.length);
+    console.log('[SelectorAll 후]',item.length);
+
+    //동적으로 변한 상황을 인식하지 못한다 = SelectorAll
+  }
 }
 
